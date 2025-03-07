d6c9a9f9eb0eb526ffbb90b47b2bad39255d195b
mever-l <mor.lempert@gmail.com>
2025-03-08 00:44:13 +0200

    feat(properties): number picker works!

--- app/src/main/java/com/idz/rentit/fragments/AddPropertyFragment.kt
+++ app/src/main/java/com/idz/rentit/fragments/AddPropertyFragment.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
+import android.widget.Toast
 import com.idz.rentIt.R
+import com.idz.rentIt.databinding.FragmentAddPropertyBinding
 
 class AddPropertyFragment : Fragment() {
+    private lateinit var viewBindings: FragmentAddPropertyBinding
+    private lateinit var numberPicker: NumberPicker
+//    private lateinit var viewModel: AddPropertyFragmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_property, container, false)
+        viewBindings = FragmentAddPropertyBinding.inflate(inflater, container, false)
+        numberPicker = viewBindings.addPropertyNumberPicker
+        configureNumberPicker()
+        activateButtonsListeners()
+        return viewBindings.root
     }
 
+    private fun configureNumberPicker() {
+        numberPicker.minValue = 100
+        numberPicker.maxValue = 10000
+        numberPicker.value = 100
+        numberPicker.wrapSelectorWheel = false
+    }
+
+    private fun activateButtonsListeners() {
+        viewBindings.addPropertyFragmentUploadBtn.setOnClickListener {
+            val selectedValue = numberPicker.value
+//            Toast.makeText(context, "Selected value: $selectedValue", Toast.LENGTH_SHORT).show()
+            // ... do something with the selectedValue ...
+        }
+    }
 }
