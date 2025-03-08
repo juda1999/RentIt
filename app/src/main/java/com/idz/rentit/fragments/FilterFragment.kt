2cbeb6972b11d3e44a96614f925bc63b97bf8095
mever-l <mor.lempert@gmail.com>
2025-03-08 13:29:13 +0200

    feat(properties): add filter page

--- app/src/main/java/com/idz/rentit/fragments/FilterFragment.kt
+++ app/src/main/java/com/idz/rentit/fragments/FilterFragment.kt
@@ -3,11 +3,22 @@ package com.idz.rentit.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
 import com.idz.rentIt.R
+import com.idz.rentIt.databinding.FragmentFilterBinding
+import com.idz.rentIt.databinding.FragmentSigninBinding
+
 
 class FilterFragment : Fragment() {
+    private lateinit var viewBindings: FragmentFilterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +28,30 @@ class FilterFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filter, container, false)
+    ): View {
+        viewBindings = FragmentFilterBinding.inflate(inflater, container, false)
+        configureMenuOptions(viewBindings.root)
+        return viewBindings.root
+    }
+
+    private fun configureMenuOptions(view: View) {
+        val parentActivity: FragmentActivity = requireActivity()
+        parentActivity.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menu.removeItem(R.id.userProfileFragment)
+                menu.removeItem(R.id.logoutMenuItem)
+                menu.removeItem(R.id.filterFragment)
+                menu.removeItem(R.id.addPropertyFragment)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return if (menuItem.itemId == android.R.id.home) {
+                    Navigation.findNavController(view).popBackStack()
+                    true
+                } else {
+                    false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
